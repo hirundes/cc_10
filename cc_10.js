b218fa74b5ba5dd8dd480a07a502f5a592ba3b77
HIRUN <hirundesilva97@gmail.com>
2025-02-18 21:50:52 -0500

    Task 2 - Created Order Class

--- cc_10.js
+++ cc_10.js
@@ -5,19 +5,19 @@ class Product {     //CREATE A CLASS Product WITH PROPERTIES NAME, ID, PRICE, ST
         this.id = id;
         this.price = price;
         this.stock = stock;
-    }
+    };
 
 getDetails() {      //ADD METHOD get(Details) THAT RETURNS FORMATTED STRING OF PRODUCT DETAILS
-    console.log(`Product: ${this.name}, ID: ${this.id}, Price: ${this.price}, Stock: $${this.stock}`);
-}
+    console.log(`Product: ${this.name}, ID: ${this.id}, Price: $${this.price}, Stock: ${this.stock}`);
+};
 
 updateStock(quantity) {     //ADD METHOD updateStock(qualtity) THAT MODIFIES STOCK LEVEL WHEN ORDER IS PLACED
     if (quantity > 0, quantity <= this.stock) {
         this.stock -= quantity;
-        console.log(`Product: ${this.name}, ID: ${this.id}, Price: ${this.price}, Stock: $${this.stock}`)
-    }
-}
-}
+        console.log(`Product: ${this.name}, ID: ${this.id}, Price: $${this.price}, Stock: ${this.stock}`)
+    };
+};
+};
 
 console.log ("---Task 1---")    //INSTANTIATE Product as prod1, LOGGED "Task 1" AND prod1.getDetails(); AND prod1.updateStock(3);
 const prod1 = new Product("Laptop", 101, 1200, 10);
@@ -26,3 +26,24 @@ prod1.getDetails();
 
 prod1.updateStock(3);
 // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 7"
+
+
+//Task 2 - Created Order Class
+class Order {       //CREATE CLASS ORDER WITH PROPERTIES ORDEID, PRODUCT, QUANTITY
+    constructor(orderId, product, quantity) {
+        this.orderId = orderId;
+        this.product = product;
+        this.quantity = quantity;
+    };
+
+getOrderDetails() {     //ADD METHOD getOrderDetails() THAT RETURNS ORDER DETAILS
+    console.log(`Order ID: ${this.orderId}, Product: ${this.product}, Quantity: ${this.quantity}`)
+};
+};
+
+console.log("---Task 2---")     //LOGGED "Task 2", getOrderDetails, and updateStock(2)
+const order1 = new Order(501, prod1, 2);
+order1.getOrderDetails(); 
+// Expected output: "Order ID: 501, Product: Laptop, Quantity: 2, Total Price: $2400"
+prod1.updateStock(2); 
+// Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5" (Stock reduced)
